7b2bd1c51a5b4ffa3b4b4ec7fa3ad134c7fdbd78
Onlyllon1 <francescoliviero15@gmail.com>
2021-11-17 12:10:31 +0100

    OPEL TO OPEN

--- Morse/Views/CardView.swift
+++ Morse/Views/CardView.swift
@@ -47,7 +47,7 @@ struct CardView: View {
                 .colorInvert()
             
             Button(action: {print("lol")}) {
-                Text("OPEL      ")
+                Text("OPEN      ")
                     .background(.white)
                     
                     .foregroundColor(.black)
